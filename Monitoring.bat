05dd178b258654fe86c30b1b90fcd29dc9df9db9
sebasgdiaz <155281907+sebasgdiaz@users.noreply.github.com>
2025-10-05 20:35:25 +1100

    Add WhatsApp integration with Twilio env variables

--- Monitoring.bat
+++ Monitoring.bat
@@ -24,6 +24,17 @@ ALERT_FILE=$LAST_ALERT_FILE/alert.txt
 LOG_FILE_TEMPORARY="$LOGS_PATH_SHELL/shell-${DATE_DAY}.log"
 FILE_LOG_HOME="/home/initial-${DATE_DAY}.txt"
 
+#Variables for EMAIL & Whatsapp
+
+EMAIL_FROM="devethosjsg.5@gmail.com"
+EMAIL_TO="devethosjsg.5@gmail.com"
+EMAIL_USER="devethosjsg.5@gmail.com"
+EMAIL_PASS="idjrmnrvsgguogqr"
+WHATSAPP_TO="whatsapp:+61415751673"
+WHATSAPP_FROM="whatsapp:+14155238886"
+ACCOUNT_SID=$TWILIO_ACCOUNT_SID
+AUTH_TOKEN=$TWILIO_AUTH_TOKEN
+
 if [[ ! -f "$FILE_LOG_HOME" ]]; then 
     touch "$FILE_LOG_HOME" && chmod -R 755 "$FILE_LOG_HOME" 2>&1
 else
@@ -187,14 +198,18 @@ fi
 
 send_mail(){
 
+ local error
+
+ ###########################Email###############################
+
 	# The template is attached and the email is sent
 	echo "Enviando correo " $MONITOR_PATH/Alerta-$1.txt;
 
-	error=$(timeout 40s curl --url 'smtps://smtp.gmail.com:465' \
-	--ssl-reqd --mail-from 'devethosjsg.5@gmail.com' \
-       	--mail-rcpt 'devethosjsg.5@gmail.com' \
+		error=$(timeout 40s curl --url 'smtps://smtp.gmail.com:465' \
+	--ssl-reqd --mail-from "$EMAIL_FROM" \
+       	--mail-rcpt "$EMAIL_TO" \
        	--upload-file $MONITOR_PATH/Alerta-$1.txt \
-	--user 'devethosjsg.5@gmail.com:idjrmnrvsgguogqr' \
+	--user "$EMAIL_USER:$EMAIL_PASS" \
 	--insecure \
 	--connect-timeout 10 \
 	--max-time 30 \ 
@@ -205,6 +220,23 @@ send_mail(){
 	else
    		echo "ERROR: Failed to send mail: $error" >> $LOG_FILE_SHELL
 	fi
+
+###########################Whatsapp###############################
+
+    echo "Enviando WhatsApp alerta $1"
+
+    error=$(curl -s -X POST "https://api.twilio.com/2010-04-01/Accounts/$ACCOUNT_SID/Messages.json" \ 
+    --data-urlencode "To=$WHATSAPP_TO" \
+    --data-urlencode "From=$WHATSAPP_FROM" \
+    --data-urlencode "Body=$MESSAGE" \
+    -u $ACCOUNT_SID:$AUTH_TOKEN)
+
+	if [ $? -eq 0  ]; then
+		echo "INFO: The message was sent successfully" >> $LOG_FILE_SHELL
+	else
+   		echo "ERROR: Failed to send message: $error" >> $LOG_FILE_SHELL
+	fi
+
 }
 
 #Variable declaration to store RAM, DISK, and CPU usage
